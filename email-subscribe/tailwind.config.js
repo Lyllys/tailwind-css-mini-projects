5b9100f2d169be2fb8025f868e3438157406ce4d
Lyllys de Souza Galhardo <lyllysgalhardo@gmail.com>
2024-05-25 16:04:50 -0400

    feature/email-subscription-card - Adding email subscription form

---
+++ email-subscribe/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // watch for any tailwind classes in the html and js files
+  content: ['./*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
