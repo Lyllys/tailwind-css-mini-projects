e3d771a10fbabc897d8e9fc65cc15be297b7ef2a
Lyllys de Souza Galhardo <lyllysgalhardo@gmail.com>
2024-05-26 11:11:44 -0400

    feature/image-gallery - Creating the image gallery with a menu, search bar and hover effects.

--- email-subscribe/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  // watch for any tailwind classes in the html and js files
-  content: ['./*.html'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
